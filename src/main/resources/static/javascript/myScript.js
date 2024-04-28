ec67d2d61ff7710cf12fb35ebc111d1c11b81cb5
EvgheniAdvahov <evghenii.88@gmail.com>
2024-04-28 20:34:07 +0300

    small fixes

--- src/main/resources/static/javascript/myScript.js
+++ src/main/resources/static/javascript/myScript.js
@@ -4,33 +4,10 @@ $(document).ready(function () {
         {
             scrollX: true,
             buttons: [
-                "copy", "csv", "excel", "print"
+                "copy", "csv", "excel", "print", 'colvis'
             ]
         }
     );
     table.buttons().container()
         .appendTo( $('.my-buttons' ) );
 });
-
-// $(document).ready(function (){
-//
-//     var table = $('#example').DataTable({
-//
-//         buttons:['copy', 'csv', 'excel', 'pdf', 'print']
-//     });
-//
-//     table.buttons().container()
-//         .appendTo('#example_wrapper .col-md-6:eq(0)');
-// });
-
-
-
-
-// var table = $('#example').DataTable( {
-//     buttons: [
-//         'copy', 'excel', 'pdf'
-//     ]
-// } );
-
-// table.buttons().container()
-//     .appendTo( $('.col-sm-6:eq(0)', table.table().container() ) );
