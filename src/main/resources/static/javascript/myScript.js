b4b72b867695ee60eb70507738ef46e42050b3b0
EvgheniAdvahov <evghenii.88@gmail.com>
2024-04-28 20:09:20 +0300

    export and horizontal scroll

--- src/main/resources/static/javascript/myScript.js
+++ src/main/resources/static/javascript/myScript.js
@@ -1,7 +1,36 @@
-new DataTable('#example', {
-    layout: {
-        topStart: {
-            buttons: ['copy', 'csv', 'excel', 'print']
+$(document).ready(function () {
+
+    var table = $('#example').DataTable(
+        {
+            scrollX: true,
+            buttons: [
+                "copy", "csv", "excel", "print"
+            ]
         }
-    }
+    );
+    table.buttons().container()
+        .appendTo( $('.my-buttons' ) );
 });
+
+// $(document).ready(function (){
+//
+//     var table = $('#example').DataTable({
+//
+//         buttons:['copy', 'csv', 'excel', 'pdf', 'print']
+//     });
+//
+//     table.buttons().container()
+//         .appendTo('#example_wrapper .col-md-6:eq(0)');
+// });
+
+
+
+
+// var table = $('#example').DataTable( {
+//     buttons: [
+//         'copy', 'excel', 'pdf'
+//     ]
+// } );
+
+// table.buttons().container()
+//     .appendTo( $('.col-sm-6:eq(0)', table.table().container() ) );
