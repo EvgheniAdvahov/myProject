9488e4555bc5a0f661653bb0385dee99e03ae3ff
EvgheniAdvahov <evghenii.88@gmail.com>
2024-04-29 12:07:29 +0300

    visual fixes

--- src/main/resources/static/javascript/myScript.js
+++ src/main/resources/static/javascript/myScript.js
@@ -1,13 +1,36 @@
 $(document).ready(function () {
 
-    var table = $('#example').DataTable(
-        {
-            scrollX: true,
-            buttons: [
-                "copy", "csv", "excel", "print", 'colvis'
-            ]
+    var table = $('#example').DataTable({
+        scrollX: true,
+        buttons: [
+            "copy", "csv", "excel", "print", 'colvis'
+        ],
+        initComplete: function () {
+            this.api().columns([2,3,4]).every(function () {
+                let column = this;
+
+                // Create select element
+                let select = document.createElement('select');
+                select.add(new Option(''));
+                column.footer().replaceChildren(select);
+
+                // Apply listener for user change in value
+                select.addEventListener('change', function () {
+                    column.search(select.value, { exact: true }).draw();
+                });
+
+                $( select ).click( function(e) {
+                    e.stopPropagation();
+                });
+
+                // Add list of options
+                column.data().unique().sort().each(function (d, j) {
+                    select.add(new Option(d));
+                });
+            });
         }
-    );
-    table.buttons().container()
-        .appendTo( $('.my-buttons' ) );
+    });
+
+    table.buttons().container().appendTo($('.my-buttons'));
+
 });
