471be8b374f4fee30964c06e5cada93ded7a467e
EvgheniAdvahov <evghenii.88@gmail.com>
2024-05-16 23:37:53 +0300

    itemdta type fixes

--- src/main/java/com/myProject/myProject/model/ItemDto.java
+++ src/main/java/com/myProject/myProject/model/ItemDto.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class ItemDto {
 
     @NotEmpty(message = "Name is required")
-    @Size(max = 7, message = "Name couldn't exceed 7 characters")
+    @Size(max = 7, message = "Name can't exceed 7 characters")
     private String name;
 
     private String manufacturer;
@@ -28,7 +28,7 @@ public class ItemDto {
 
     private Integer inventoryNumber;
 
-    @Size(max = 300, message = "The description can't exceed 300 characters")
+    @Size(max = 300, message = "Description can't exceed 300 characters")
     private String description;
 
     private MultipartFile imageFile;
